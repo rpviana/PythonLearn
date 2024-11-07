e7d188fb312cce3f4a813f21347cb9016785736d
Rodrigo Viana <142672337+rpviana@users.noreply.github.com>
2024-11-07 12:29:10 +0000

    Add files via upload

---
+++ Jogo/biblio.py
@@ -0,0 +1,7 @@
+#from colorama import Fore, Back #para pintar as naves, tiros da mesma cor que a nave
+from getpass import getpass #passar de ronda por ex: a pessoa acaba a ronda, ve pontuacao e quando quiser ir para a proxima clica ENTER
+from random import randrange, randint #tiros aleatorios
+
+print("Texto")#Meter o texto vermelho, e depois parar
+getpass("ENTER para continuar ...")#para 
+print("Outro Texto")
